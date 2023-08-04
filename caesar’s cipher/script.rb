bd71f63a5321b381e91acfbc57eb02ec9bc1463f
Opeyemi Adetifa <opeyemiadetifa17@gmail.com>
2023-08-04 23:21:18 +0100

    Add function for Caeser's cipher

---
+++ caesar’s cipher/script.rb
@@ -0,0 +1,19 @@
+def caesar_cipher(text, shift)
+    encrypted_text = ""
+  
+    text.each_char do |char|
+      if char.match(/[a-zA-Z]/)
+        base = char.match(/[a-z]/) ? 'a' : 'A'
+        encrypted_char = (base.ord + (char.ord - base.ord + shift) % 26).chr
+        encrypted_text << encrypted_char
+      else
+        encrypted_text << char
+      end
+    end
+  
+    encrypted_text
+end
+  
+text = "What a string"
+shift = 5
+encrypted_text = caesar_cipher(text, shift)
